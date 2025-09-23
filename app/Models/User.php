6b3b4e05d8a78f860d056ee7d8152ed8b3a11edd
pjcruz-dev <peterjosephcruz01@gmail.com>
2025-09-23 16:36:39 +0800

    fix remember token

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable
         'status',
         'phone',
         'job_title',
+        'remember_token',
     ];
 
     /**
@@ -44,6 +45,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
+        'remember_token',
     ];
 
     /**
